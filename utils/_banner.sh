0e89cad9f7547fba9b3fe4c203d101f01a8402ef
thigranpeio <111096666+thigranpeio@users.noreply.github.com>
2024-02-23 01:23:44 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -23,10 +23,11 @@ print_banner() {
 
 printf "${CYAN_LIGHT}";
 
-printf "${CYAN_LIGHT}INSTALAÇÃO DO TZAP MULTI ATENDIMENTO\n"
 
+printf ${CYAN_LIGHT}" INSTALACAO DO TZAP MULTIATENDIMENTO\n";  
 
-  printf "            \033[1;33m        by Thiago Romanini\n";
+                                                                                                                                                         
+  printf "            \033[1;33m        BY THIAGO ROMANINI\n";
   printf "${NC}";
 
   printf "\n"
