0b4d64d4ae8d98659503f90b9f7131131d132f23
thigranpeio <111096666+thigranpeio@users.noreply.github.com>
2024-02-23 01:20:01 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -24,7 +24,6 @@ print_banner() {
 printf "${CYAN_LIGHT}";
 
 printf "${CYAN_LIGHT}INSTALAÇÃO DO TZAP MULTI ATENDIMENTO\n"
-printf "${CYAN_LIGHT}-----------------------------------------------------------------------------------\n"
 printf "${CYAN_LIGHT}8888888 8888888888            8888888888',8888'        .8.          8 888888888o   \n"
 printf "${CYAN_LIGHT}      8 8888                         ,8',8888'        .888.         8 8888    `88. \n"
 printf "${CYAN_LIGHT}      8 8888                        ,8',8888'        :88888.        8 8888     `88 \n"
@@ -35,8 +34,7 @@ printf "${CYAN_LIGHT}      8 8888                    ,8',8888'        .8' `8. `8
 printf "${CYAN_LIGHT}      8 8888                   ,8',8888'        .8'   `8. `88888.   8 8888         \n"
 printf "${CYAN_LIGHT}      8 8888                  ,8',8888'        .888888888. `88888.  8 8888         \n"
 printf "${CYAN_LIGHT}      8 8888                 ,8',8888888888888.8'       \`8. \`888888 8888         \n"
-printf "${CYAN_LIGHT}-----------------------------------------------------------------------------------\n"
-                                                                                                                                                         
+
   printf "            \033[1;33m        by Thiago Romanini\n";
   printf "${NC}";
 
